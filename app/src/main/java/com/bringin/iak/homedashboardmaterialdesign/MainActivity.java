0c144651a5ee59972be88ec177eb45ace46bb700
yogiyulianto <yogi.yulianto@students.amikom.ac.id>
2018-05-19 22:34:35 +0700

    add intent in all layout

--- app/src/main/java/com/bringin/iak/homedashboardmaterialdesign/MainActivity.java
+++ app/src/main/java/com/bringin/iak/homedashboardmaterialdesign/MainActivity.java
@@ -1,13 +1,18 @@
 package com.bringin.iak.homedashboardmaterialdesign;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.view.Menu;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
 
     CardView cvPoint,cvIdea, cvBank, cvFile;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +23,42 @@ public class MainActivity extends AppCompatActivity {
         cvBank = findViewById(R.id.banking_view);
         cvFile = findViewById(R.id.file_view);
 
-        cvPoint.setOnClickListener(this);
-        cvIdea.setOnClickListener(this);
-        cvBank.setOnClickListener(this);
-        cvFile.setOnClickListener(this);
+        cvPoint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Point.class);
+                startActivity(intent);
+            }
+        });
+        cvIdea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Idea.class);
+                startActivity(intent);
+            }
+
+        });
+        cvBank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Bank.class);
+                startActivity(intent);
+            }
+
+        });
+        cvFile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, File.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+
+
 
     }
+
 }
